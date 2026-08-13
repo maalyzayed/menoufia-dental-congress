6abb9467f3766cb2e30198edd5c9576b64835bce
Maaly Salem <50291965+maalyzayed@users.noreply.github.com>
2026-08-13 11:44:51 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "menoufia-dental-congress-v1";
+
+const FILES_TO_CACHE = [
+  "/menoufia-dental-congress/",
+  "/menoufia-dental-congress/index.html",
+  "/menoufia-dental-congress/registration.html",
+  "/menoufia-dental-congress/checkin.html",
+  "/menoufia-dental-congress/speakers.html",
+  "/menoufia-dental-congress/manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
